b0e516fdfeb65a17d750f587bc199a2927997e75
emakepuska <emakepuska5@gmail.com>
2017-11-01 17:03:01 +0100

    updates

--- app/src/main/java/com/enterprise/lu/uni/notebook/app/activity/ExportImportActivity.java
+++ app/src/main/java/com/enterprise/lu/uni/notebook/app/activity/ExportImportActivity.java
@@ -1,11 +1,18 @@
 package com.enterprise.lu.uni.notebook.app.activity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.enterprise.lu.uni.notebook.R;
+
+import java.io.File;
+import java.io.FileWriter;
 
 public class ExportImportActivity extends AppCompatActivity {
 
@@ -20,8 +27,36 @@ public class ExportImportActivity extends AppCompatActivity {
         exportButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                File exportDir = new File(Environment.getExternalStorageDirectory(), "");
+                if (!exportDir.exists())
+                {
+                    exportDir.mkdirs();
+                }
 
+                File file = new File(exportDir, "csvname.csv");
+                try
+                {
+                    DBHelper mHelper= new DBHelper(getApplicationContext());
+                    file.createNewFile();
+                    CSVWriter csvWrite = new CSVWriter(new FileWriter(file));
+                    SQLiteDatabase db = mHelper.getReadableDatabase();
+                    Cursor curCSV = db.rawQuery("SELECT * FROM NewWord",null);
+                    csvWrite.writeNext(curCSV.getColumnNames());
+                    while(curCSV.moveToNext())
+                    {
+                        //Which column you want to export
+                        String arrStr[] ={curCSV.getString(0),curCSV.getString(1), curCSV.getString(2)};
+                        csvWrite.writeNext(arrStr);
+                    }
+                    csvWrite.close();
+                    curCSV.close();
+                }
+                catch(Exception sqlEx)
+                {
+                    Log.e("MainActivity", sqlEx.getMessage(), sqlEx);
+                }
             }
+
         });
 
         importButton.setOnClickListener(new View.OnClickListener() {
