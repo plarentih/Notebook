223e530ae7726cce1b5b9e62c699e17e122bc444
Plarent Haxhidauti <plarentih@gmail.com>
2017-11-04 14:05:18 +0100

    updates

--- app/src/main/java/com/enterprise/lu/uni/notebook/app/activity/AddDomainActivity.java
+++ app/src/main/java/com/enterprise/lu/uni/notebook/app/activity/AddDomainActivity.java
@@ -16,21 +16,39 @@ public class AddDomainActivity extends AppCompatActivity {
     private Button addButton;
     private EditText editTextDomainName;
 
+    private Domain domainToEdit;
+    private long domainToEditId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_domain);
         initializeWidgets();
 
+        domainToEdit = (Domain) getIntent().getSerializableExtra("EDIT_DOMAIN");
+        domainToEditId = getIntent().getLongExtra("EDIT_DOMAIN_ID", -1);
+        if(domainToEdit != null){
+            editTextDomainName.setText(domainToEdit.getDomainName());
+        }
+
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Editable typedDomainName = editTextDomainName.getText();
-                Domain newDomain = new Domain();
-                newDomain.domainName = typedDomainName.toString();
-                newDomain.save();
-                setResult(RESULT_OK);
-                finish();
+                if(domainToEdit != null){
+                    Domain domain = Domain.load(Domain.class, domainToEditId);
+                    domain.domainName = typedDomainName.toString();
+                    domain.save();
+                    setResult(RESULT_OK);
+                    finish();
+                }
+                if(domainToEdit == null){
+                    Domain newDomain = new Domain();
+                    newDomain.domainName = typedDomainName.toString();
+                    newDomain.save();
+                    setResult(RESULT_OK);
+                    finish();
+                }
             }
         });
     }
--- app/src/main/java/com/enterprise/lu/uni/notebook/app/activity/DomainActivity.java
+++ app/src/main/java/com/enterprise/lu/uni/notebook/app/activity/DomainActivity.java
@@ -1,17 +1,22 @@
 package com.enterprise.lu.uni.notebook.app.activity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.activeandroid.query.Select;
 import com.enterprise.lu.uni.notebook.R;
 import com.enterprise.lu.uni.notebook.app.model.Domain;
 import com.enterprise.lu.uni.notebook.app.model.NewWord;
+import com.enterprise.lu.uni.notebook.app.tools.UIHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -44,6 +49,65 @@ public class DomainActivity extends AppCompatActivity {
                 startActivityForResult(intent, REQUEST_CODE_ADD_DOMAIN);
             }
         });
+
+        domainListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                final int internalPosition = position;
+
+                final AlertDialog.Builder builder = new AlertDialog.Builder(DomainActivity.this);
+                builder.setTitle("Edit/Delete");
+                builder.setMessage("Do you want to edit or delete this word?");
+                builder.setPositiveButton("Edit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String selectedDomainName = arrayAdapter.getItem(internalPosition);
+                        Domain selectedDomain = getDomainFromName(selectedDomainName);
+                        Intent intent = new Intent(getBaseContext(), AddDomainActivity.class);
+                        intent.putExtra("EDIT_DOMAIN", selectedDomain);
+                        intent.putExtra("EDIT_DOMAIN_ID", selectedDomain.getId());
+                        startActivityForResult(intent, REQUEST_CODE_ADD_DOMAIN);
+                    }
+                });
+                builder.setNegativeButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String selectedDomainName = arrayAdapter.getItem(internalPosition);
+                        Domain selectedDomain = getDomainFromName(selectedDomainName);
+                        if(domainExistsInWordTable(selectedDomain)){
+                            UIHelper.showDialog(DomainActivity.this, "Attention!", "You can not delete this domain " +
+                                    "because it is used in your word list.", "OK", null);
+                        }else {
+                            selectedDomain.delete();
+                            getDomainNames();
+                            arrayAdapter = new ArrayAdapter<String>(getBaseContext(),
+                                    android.R.layout.simple_list_item_1, domainNames);
+                            domainListView.setAdapter(arrayAdapter);
+                            Toast.makeText(getBaseContext(), "Domain is deleted!", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+                builder.setCancelable(true);
+                builder.show();
+            }
+        });
+    }
+
+    private boolean domainExistsInWordTable(Domain domain){
+        for(int index = 0; index < getAllNewWords().size(); index++){
+            if(domain.equals(getAllNewWords().get(index).getDomain())){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static Domain getDomainFromName(String domainName){
+        return new Select()
+                .from(Domain.class)
+                .where("Name = ?", domainName)
+                .executeSingle();
     }
 
     private void getDomainNames(){
@@ -56,6 +120,12 @@ public class DomainActivity extends AppCompatActivity {
     public static List<Domain> getAllDomains(){
         return new Select()
                 .from(Domain.class)
+                .execute();
+    }
+
+    public static List<NewWord> getAllNewWords(){
+        return new Select()
+                .from(NewWord.class)
                 .execute();
     }
 
