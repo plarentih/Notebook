a02ad6967c5b530f8766980ee3eb1c659605b563
emakepuska <emakepuska5@gmail.com>
2017-11-06 20:21:28 +0100

    updates

--- app/src/main/java/com/enterprise/lu/uni/notebook/app/activity/NotebookActivity.java
+++ app/src/main/java/com/enterprise/lu/uni/notebook/app/activity/NotebookActivity.java
@@ -21,6 +21,7 @@ public class NotebookActivity extends AppCompatActivity {
             "P", "Q", "R", "S", "T",
             "U", "V", "W", "X", "Y", "Z"};
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +30,9 @@ public class NotebookActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         gridView = (GridView)findViewById(R.id.gridView);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, letters);
+        gridView.setAdapter(adapter);
+
+
         gridView.setAdapter(adapter);
 
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
